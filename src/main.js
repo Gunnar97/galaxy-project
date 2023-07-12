714f90147668fe2ad1510dea8be80e665214234e
Dmytro Prontenko <dptontenko@gmail.com>
2023-07-12 09:28:01 +0200

    layout + styles + section img + main.js
    Додана розмітка секції, трішки стилів, доданий лінк js і внесений скрипт в main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 'use strict';
 
-// =============SWIPER FOR YACHT BLOCK=========
-
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+  direction: 'horizontal', // "horizontal", "vertical"
+  loop: true, // true - круговий слайдер, false - зкінцевим положенням
+  // speed: 600, // швидкість перемикання слайду
+  effect: 'fade', // slider, cards, coverflow, flip, fade, cube
+  // initialSlide: 1, // початок з 2-го слайду
+  // freemode: true, // дозволяє перетаскувати мишкою
+  slidesPerView: 'auto', // кількість активних слайдів
+  // centeredSlides: true, // центрування слайду
 
   // Navigation arrows
   navigation: {
@@ -18,8 +17,29 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+  breapoints: {
+    375: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+  },
+});
+
+const mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
 });
