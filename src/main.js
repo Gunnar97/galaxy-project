5a45f055ffb1d445b474a95a2dc6ba0a462eb564
Dmytro Prontenko <dptontenko@gmail.com>
2023-07-12 17:15:24 +0200

    backup

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const swiper = new Swiper('.swiper', {
   effect: 'fade', // slider, cards, coverflow, flip, fade, cube
   // initialSlide: 1, // початок з 2-го слайду
   // freemode: true, // дозволяє перетаскувати мишкою
-  slidesPerView: 'auto', // кількість активних слайдів
+  slidesPerView: 2, // кількість активних слайдів
   // centeredSlides: true, // центрування слайду
 
   // Navigation arrows
@@ -17,29 +17,19 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-  breapoints: {
-    375: {
-      slidesPerView: 1,
-    },
+  // breapoints: {
+  //   375: {
+  //     slidesPerView: 1,
+  //   },
 
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 16,
-    },
+  //   768: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 16,
+  //   },
 
-    1280: {
-      slidesPerView: 3,
-      spaceBetween: 16,
-    },
-  },
-});
-
-const mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+  //   1280: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 16,
+  //   },
+  // },
 });
