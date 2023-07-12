c06d3b7f94759ce2e94d7402c43611c9a00379d4
Dmytro Prontenko <dptontenko@gmail.com>
2023-07-12 18:21:15 +0200

    slider done
    натягнув слайдер

--- src/main.js
+++ src/main.js
@@ -2,34 +2,34 @@
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'horizontal', // "horizontal", "vertical"
-  loop: true, // true - круговий слайдер, false - зкінцевим положенням
+  // direction: "horizontal", // "horizontal", "vertical"
   // speed: 600, // швидкість перемикання слайду
-  effect: 'fade', // slider, cards, coverflow, flip, fade, cube
+  // effect: "fade", // slider, cards, coverflow, flip, fade, cube
   // initialSlide: 1, // початок з 2-го слайду
   // freemode: true, // дозволяє перетаскувати мишкою
+  spaceBetween: 16,
+  loop: true, // true - круговий слайдер, false - зкінцевим положенням
   slidesPerView: 2, // кількість активних слайдів
-  // centeredSlides: true, // центрування слайду
-
+  centeredSlides: false, // центрування слайду
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakpoints: {
+    1280: {
+      slidesPerView: 3,
+      initialSlide: 1,
+      spaceBetween: 16,
+    },
 
-  // breapoints: {
-  //   375: {
-  //     slidesPerView: 1,
-  //   },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
 
-  //   768: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 16,
-  //   },
-
-  //   1280: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 16,
-  //   },
-  // },
+    375: {
+      slidesPerView: 1,
+    },
+  },
 });
