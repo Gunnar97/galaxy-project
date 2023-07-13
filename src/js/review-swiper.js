efb9ab0ad32be6f5b313e80bc7f7cf7d614c4b03
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-07-14 01:11:51 +0300

    Create review-swiper.js

---
+++ src/js/review-swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  clickable: true,
+  //   loop: true,
+  //   slideToClickedSlide: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
