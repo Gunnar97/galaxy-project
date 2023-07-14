b1cc3619e50ec9981396235f6ec88456d6d4c8a3
Gunnar97 <vshatov7982@gmail.com>
2023-07-14 15:38:19 +0300

    fixed js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,33 +1,46 @@
-export const swiper = new Swiper('.swiper', {
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const yachtsSlider = new Swiper('#yachts-slider', {
   // Optional parameters
   // direction: "horizontal", // "horizontal", "vertical"
   // speed: 600, // швидкість перемикання слайду
   // effect: "fade", // slider, cards, coverflow, flip, fade, cube
-  // initialSlide: 1, // початок з 2-го слайду
+  initialSlide: 0, // початок з 2-го слайду
   // freemode: true, // дозволяє перетаскувати мишкою
   spaceBetween: 16,
   loop: true, // true - круговий слайдер, false - зкінцевим положенням
-  slidesPerView: 1, // кількість активних слайдів
-  centeredSlides: false, // центрування слайду
+
+  //centeredSlides: false, // центрування слайду
+  modules: [Navigation],
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   breakpoints: {
-    1280: {
-      slidesPerView: 3,
-      initialSlide: 1,
-      spaceBetween: 16,
-    },
-
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 16,
-    },
-
     375: {
       slidesPerView: 1,
     },
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const reviewsSlider = new Swiper('#reviews-slider', {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  clickable: true,
+
+  modules: [Pagination],
+  pagination: {
+    el: '.swiper-pagination',
   },
 });
